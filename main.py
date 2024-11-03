298de01b61caf62bc4eb7914fc56399c5bd2ddd8
Дарья <hzxz4610@gmail.com>
2024-11-03 15:08:17 +0300

    1

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-x = input("Введи строку: ")
-glas = "аеёиоуыэюя"
-soglas = "бвгджзйклмнпрстфхцчшщ"
+x = input("Введите словов на русском: ")
+glas = ["у","е","ы","а","о","э","я","и","ю", "ё"]
+soglas = ["й","ц","к","н","г","ш","щ","з","х","ъ","ж","д","л","р","п","в","ф","ч","с","м","т","б","ь",]
+countsoglas = 0
+countglas = 0
+for i in x:
+    for j in glas:
+        if i == j:
+            countglas +=1
+            
+for i in x:
+    for j in soglas:
+        if i == j:
+            countsoglas +=1
+print(len(x))
+print(countsoglas)
+print (countglas)
 
-glas_count = 0
-soglas_count = 0
 
-for i in x.lower():
-  if i in glas:
-    glas_count += 1
-  elif i in soglas:
-    soglas_count += 1
-
-print("Длина строки ", len(x))
-print("Гласных в строке ", glas_count)
-print("Согласных в строке ",  soglas_count)
