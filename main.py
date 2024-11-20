5a83d99ead63bae250756c01424ee2c9c157fa13
Дарья <hzxz4610@gmail.com>
2024-11-20 16:27:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ glas = ["у","е","ы","а","о","э","я","и","ю", "ё"]
 soglas = ["б", "в", "г", "д", "ж", "з", "й", "к", "л", "м", "р", "н", "п", "c", "т", "ф", "ч", "ц", "щ", "ш", "х", "ь"]
 countsoglas = 0
 countglas = 0
+
 for i in x:
-    for j in glas:
-        if i == j:
-            countglas +=1
-    for k in soglas:
-        if i == k:
-            countsoglas +=1
-            
+    if i in glas :
+        countglas +=1
+for k in x:
+    if k in soglas:
+        countsoglas +=1
+        
 print(len(x))
 print(countsoglas)
 print (countglas)
