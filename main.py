45d4aa68030743e81ae3c2f840dd0274c56412e1
Дарья <hzxz4610@gmail.com>
2024-11-20 15:49:44 +0300

    1

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 x = input("Введите словов на русском: ")
 glas = ["у","е","ы","а","о","э","я","и","ю", "ё"]
+soglas = ["б", "в", "г", "д", "ж", "з", "й", "к", "л", "м", "р", "н", "п", "c", "т", "ф", "ч", "ц", "щ", "ш", "х", "ь"]
 countsoglas = 0
 countglas = 0
 for i in x:
     for j in glas:
         if i == j:
             countglas +=1
-        else:
+    for k in soglas:
+        if i == k:
             countsoglas +=1
             
 print(len(x))
 print(countsoglas)
 print (countglas)
-
-
